3ffa3b07cff3901f655df21c303192d20ccf00c4
bara-codes <198811843+bara-codes@users.noreply.github.com>
2025-09-15 13:51:05 -0400

    Lewis Arithmetic 2.8 - Distance Program

--- src/main/java/DistanceMultiple.java
+++ src/main/java/DistanceMultiple.java
@@ -1,39 +1,58 @@
-/*
- * TODO: APCS-A Distance Program
- *
- * Write a program that:
- *  1. Asks the user how many inputs (pairs of points) they want to enter.
- *  2. For each input, prompt them to enter two points in the format:
- *         (x1,y1)(x2,y2)
- *     Example: (2,3)(7,3)
- *
- *  3. Use Scanner with a custom delimiter:
- *     This should ignore parentheses, commas, and spaces so only numbers remain.
- *
- *  4. Extract x1, y1, x2, y2 as doubles.
- *
- *  5. Compute the distance between the two points:
- *
- *  6. Print the results
- * 
- *  7. Repeat until all inputs have been processed.
- *
- * Example Inputs:
- *  How many inputs? 3
- *  Enter input 1 (like (x1,y1)(x2,y2)): (2,3)(7,3)
- *  Enter input 2 (like (x1,y1)(x2,y2)): (4,5)(8,8)
- *  Enter input 3 (like (x1,y1)(x2,y2)): (-1,-2)(0,7)
- *
- */
+// NAME						:	Bara Omaira
+// GROUP					:	APCS-A
+// LAST MODIFIED			:	11 September 2025
+// PROBLEM ID				:	Lewis 2.8: Distance Program
+// PROBLEM DESCRIPTION		:	Prompt the user for two coordinate points
+//							:	and find the distance between them.
+// SOURCES I USED			:	Java Textbook, Mr. Houtrouw, Mr. Afsal
+// PEOPLE I HELPED			:	N/A
+// PEOPLE WHO HELPED ME		:	Mr. Houtrouw
 
-
+// import a scanner
 import java.util.Scanner;
 
+public class DistanceMultiple
+{
+    public static void main(String[] args)
+    {
+        // create a scanner object
+        Scanner sc = new Scanner(System.in);
 
-public class DistanceMultiple {
-    public static void main(String[] args) {
+        // declare variables
+        double sum, product, average;
 
+        // signature
+        System.out.print("Bara Omaira\nAPCS-A\nLewis 2.8: Distance Program\n\n\n");
 
+        // ask the user how many times they want to run the program
+        System.out.println("How many times would you like to run the program?  ");
+        int numRuns = sc.nextInt();
+
+        for(int j=0; j<numRuns; j++)
+        {
+
+            // read in input
+            System.out.println("\nEnter 3 numbers:  ");
+            double num1 = sc.nextFloat();
+            double num2 = sc.nextFloat();
+            double num3 = sc.nextFloat();
+
+            // calculate sum
+            sum = num1 + num2 + num3;
+
+            // calculate product
+            product = num1 * num2 * num3;
+
+            // calculate average
+            average = (num1 + num2 + num3)/3;
+
+            // print the results
+            System.lineSeparator();
+            System.out.println("Sum: " + sum);
+            System.out.println("Product: " + product);
+            System.out.println("Average: " + average);
+            System.lineSeparator();
+            System.lineSeparator();
+        }
     }
-
 }
